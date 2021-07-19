764227ca3ac8be9c448e23c413b31e60058e6446
Ramprabhu248 <ramprabhu248@gmail.com>
2021-07-19 18:40:46 +0530

    message

---
+++ src/test/java/org/mia/Abcd.java
@@ -0,0 +1,27 @@
+package org.mia;
+
+import org.testng.annotations.Test;
+
+public class Abcd {
+	@Test
+	private void tc0() {
+		System.out.println("one");
+	}
+	@Test
+	private void tc1() {
+		System.out.println("two");
+	}
+	@Test
+	private void tc2() {
+		System.out.println("three");
+	}
+	@Test
+	private void tc3() {
+		System.out.println("four");
+	}
+	@Test
+	private void tc4() {
+		System.out.println("five");
+	}
+
+}
